9de2a02fb98decae9a0a75a8ee9f2968a77cf6d6
Allan Omondi <aomondi@strathmore.edu>
2024-11-04 07:59:20 +0300

    Update lab_submission.sql

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,1 +1,7 @@
--- CREATE EVN_average_customer_waiting_time_every_1_hour
+-- (i) EXPLAIN output of any query of your choice that involves the “postalCode” of the “customers” relation in the WHERE clause and that does not use any index to retrieve its results.
+
+
+-- (ii) Code to create a B+ Tree index on the column “postalCode” in the table “customers”. 
+
+
+-- (iii) EXPLAIN output of the same query showing that it is using the created index to retrieve the results.
